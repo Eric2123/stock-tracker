da812842b9c2d5baa8650c51d6f887ca7da3d645
Eric2123 <138998067+Eric2123@users.noreply.github.com>
2025-11-24 19:23:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def process_data(file):
                 "Company Name": row["Company Name"],
                 "Ticker": ticker,
                 "Record Price": row["Record Price"],
-                "Current Price": round(current)}
+                "Current Price": round(current)
+            })
